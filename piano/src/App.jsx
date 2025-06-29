27242a378561d3cb108819c74b1cab7451eb57d0
hyeonseo1109 <alsgustj2@naver.com>
2025-06-29 22:50:43 +0900

    css 임시저장

--- piano/src/App.jsx
+++ piano/src/App.jsx
@@ -1,23 +1,68 @@
-import { useState } from 'react'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <h1>메롱</h1>
+      <div className="pack">
+        <div className="octave1">
+          <div className="black">
+            <div className="blackKeys" id="lowBlack1"></div>
+            <div className="blackKeys" id="lowBlack2"></div>
+            <div className="blackKeys" id="lowBlack3"></div>
+            <div className="blackKeys" id="lowBlack4"></div>
+            <div className="blackKeys" id="lowBlack5"></div>
+          </div>
+          <div className="white">
+            <div className="whiteKeys"></div>
+            <div className="whiteKeys"></div>
+            <div className="whiteKeys"></div>
+            <div className="whiteKeys"></div>
+            <div className="whiteKeys"></div>
+            <div className="whiteKeys"></div>
+            <div className="whiteKeys"></div>
+          </div>
+        </div>
+
+        <div className="octave2">
+          <div className="black">
+            <div className="blackKeys" id="midBlack1"></div>
+            <div className="blackKeys" id="midBlack2"></div>
+            <div className="blackKeys" id="midBlack3"></div>
+            <div className="blackKeys" id="midBlack4"></div>
+            <div className="blackKeys" id="midBlack5"></div>
+          </div>
+          <div className="white">
+            <div className="whiteKeys"></div>
+            <div className="whiteKeys"></div>
+            <div className="whiteKeys"></div>
+            <div className="whiteKeys"></div>
+            <div className="whiteKeys"></div>
+            <div className="whiteKeys"></div>
+            <div className="whiteKeys"></div>
+          </div>
+        </div>
+
+        <div className="octave3">
+          <div className="black">
+            <div className="blackKeys" id="highBlack1"></div>
+            <div className="blackKeys" id="highBlack2"></div>
+            <div className="blackKeys" id="highBlack3"></div>
+            <div className="blackKeys" id="highBlack4"></div>
+            <div className="blackKeys" id="highBlack5"></div>
+          </div>
+          <div className="white">
+            <div className="whiteKeys"></div>
+            <div className="whiteKeys"></div>
+            <div className="whiteKeys"></div>
+            <div className="whiteKeys"></div>
+            <div className="whiteKeys"></div>
+            <div className="whiteKeys"></div>
+            <div className="whiteKeys"></div>
+          </div>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
