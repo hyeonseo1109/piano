844a1504141f72c5a2070dec89099798ac435ed1
hyeonseo1109 <alsgustj2@naver.com>
2025-06-30 00:38:36 +0900

    💄 피아노 건반 모양 구현
    낮은도~높은시까지 총 3옥타브의 피아노 건반 모양 구현함.

--- piano/src/App.jsx
+++ piano/src/App.jsx
@@ -4,15 +4,16 @@ import './App.css'
 function App() {
   return (
     <>
+      const 
       <h1>메롱</h1>
       <div className="pack">
         <div className="octave1">
           <div className="black">
-            <div className="blackKeys" id="lowBlack1"></div>
-            <div className="blackKeys" id="lowBlack2"></div>
-            <div className="blackKeys" id="lowBlack3"></div>
-            <div className="blackKeys" id="lowBlack4"></div>
-            <div className="blackKeys" id="lowBlack5"></div>
+            <div className="blackKeys black1" id="lowBlack1"></div>
+            <div className="blackKeys black2" id="lowBlack2"></div>
+            <div className="blackKeys black3" id="lowBlack3"></div>
+            <div className="blackKeys black4" id="lowBlack4"></div>
+            <div className="blackKeys black5" id="lowBlack5"></div>
           </div>
           <div className="white">
             <div className="whiteKeys"></div>
@@ -27,11 +28,11 @@ function App() {
 
         <div className="octave2">
           <div className="black">
-            <div className="blackKeys" id="midBlack1"></div>
-            <div className="blackKeys" id="midBlack2"></div>
-            <div className="blackKeys" id="midBlack3"></div>
-            <div className="blackKeys" id="midBlack4"></div>
-            <div className="blackKeys" id="midBlack5"></div>
+            <div className="blackKeys black1" id="midBlack1"></div>
+            <div className="blackKeys black2" id="midBlack2"></div>
+            <div className="blackKeys black3" id="midBlack3"></div>
+            <div className="blackKeys black4" id="midBlack4"></div>
+            <div className="blackKeys black5" id="midBlack5"></div>
           </div>
           <div className="white">
             <div className="whiteKeys"></div>
@@ -46,11 +47,11 @@ function App() {
 
         <div className="octave3">
           <div className="black">
-            <div className="blackKeys" id="highBlack1"></div>
-            <div className="blackKeys" id="highBlack2"></div>
-            <div className="blackKeys" id="highBlack3"></div>
-            <div className="blackKeys" id="highBlack4"></div>
-            <div className="blackKeys" id="highBlack5"></div>
+            <div className="blackKeys black1" id="highBlack1"></div>
+            <div className="blackKeys black2" id="highBlack2"></div>
+            <div className="blackKeys black3" id="highBlack3"></div>
+            <div className="blackKeys black4" id="highBlack4"></div>
+            <div className="blackKeys black5" id="highBlack5"></div>
           </div>
           <div className="white">
             <div className="whiteKeys"></div>
